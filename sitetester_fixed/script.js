0f686b7a2a76e994e6974c1f90bb2cc45200fc03
estudantejfla <estudantejfla@gmail.com>
2025-11-19 01:25:41 -0300

    Delete sitetester_fixed directory

--- sitetester_fixed/script.js
+++
@@ -1,9 +0,0 @@
-
-// Utilidades compartilhadas
-(function(){
-  // Marca link ativo
-  const path = location.pathname.split('/').pop();
-  document.querySelectorAll('.navbar a').forEach(a=>{
-    if(a.getAttribute('href') === path){ a.classList.add('active'); }
-  });
-})();
